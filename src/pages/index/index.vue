43395307b2b6ca86500484ba56b961a9c4cc06c4
Clay_wang <Clay_wang@vip.163.com>
2020-08-18 17:31:15 +0800

    用mpvue重写2

---
+++ src/pages/index/index.vue
@@ -0,0 +1,118 @@
+<template>
+	<div class="index">
+		<div class="search">
+			<navigator open-type="navigate" class="location" url="/pages/mappage" @click="toMapPage">{{locationName}}
+      </navigator>
+			<div class="inputWrapper">
+				<label></label>
+				<input class="searchBar" type="text" placeholder="搜索商品" />
+			</div>
+		</div>
+	</div>
+
+</template>
+
+<script>
+	import amapFile from "../../utils/amap-wx.js"
+	import{mapState,mapMutations} from 'vuex'
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		computed:{
+			...mapState(['locationName'])
+		},
+		methods: {
+			...mapMutations(['update']),
+			toMapPage(){
+				// uni.getLocation({
+				// 	type:'wgs84',
+				// 	success: (res) => {
+				// 		console.log(res)
+				// 	}
+				// })
+				wx.getSetting({
+					success: (res) => {
+						//如果没有同意授权,则openSetting
+						if (!res.authSetting['scope.userLocation']) {
+							wx.openSetting({
+								success: (xxx) => {
+									//获取授权位置信息
+									this.getCityName()
+								}
+							})
+						}else{
+							// console.log('333')
+							this.getCityName()
+							wx.navigateTo({
+								url:'/pages/mappage/main'
+							})
+						}
+					}
+				})
+			},
+			getCityName(){
+				var myAmapFun = new amapFile.AMapWX({key:'dd6e74b8b8ebbadcf32a262baa2b441c'});
+				myAmapFun.getRegeo({
+					success:(data)=>{
+						// console.log(data)
+					},
+					fail: (err)=>{
+						// console.log(err)
+						// this.cityName = '北京'
+						this.update({locationName: '北京'})
+					}
+				})
+			}
+		}
+	}
+</script>
+
+<style lang="less" scoped>
+	.search {
+    display: flex;
+		align-items: center;
+		.location {
+			width: 115rpx;
+			// padding-right: 15rpx;
+			text-align: center;
+			line-height: 80rpx;
+			font-size: 20rpx;
+			white-space: nowrap;
+			text-overflow: ellipsis;
+			overflow: hidden;
+			// border: 1px solid red;
+		}
+		.inputWrapper {
+			box-sizing: border-box;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			width: 100%;
+			background-color: #DDDDDD;
+			border-radius: 8rpx;
+			margin-right: 20rpx;
+			.searchBar {
+				width: 100%;
+				height: 56rpx;
+				box-sizing: border-box;
+			}
+			label {
+				// border: 1px solid blue;
+				margin-left: 10rpx;
+				width: 28rpx;
+				height: 28rpx;
+				background: url(../../../static/images/search.png) center;
+				background-size: 27rpx;
+				background-repeat: no-repeat;
+			}
+		}
+
+	}
+
+  .search {
+    height: 80rpx;
+  }
+</style>
